172aa355eea893906f49d5cdacf8be9965caecbb
Mrin <mrin9@yahoo.com>
2018-10-18 19:31:56 -0700

    added ui dist folder

---
+++ web-ui/dist/js/lang-en.c65747b2.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["lang-en"],{"3fab":function(a,n,e){"use strict";e.r(n);e("cadf"),e("551c"),e("097d");n["default"]={m:{product_name:"MRIN",change_lang:"Change Language",login:"LOGIN",username:"User Name",password:"Password",domain:"Domain"}}}}]);
+//# sourceMappingURL=lang-en.c65747b2.js.map
